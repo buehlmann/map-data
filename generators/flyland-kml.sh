951e98f4a4087b8c770a9a0b3a822fb56a3d0756
Benjamin Bühlmann <buehlmann@puzzle.ch>
2020-03-29 18:21:48 +0200

    Delegating to script from workflow

--- generators/flyland-kml.sh
+++ generators/flyland-kml.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-xmlstarlet sel -N "ns=http://www.opengis.net/kml/2.2" -t -v "/ns:kml/ns:Folder/ns:Folder/ns:name/." raw.kml | sort > generated/_layers-available.log
-cat generated/_layers-available.log
+mkdir -p generated
+curl 'http://www.flyland.ch/fl_php/fl_php_Downloader.php?case=1001' --compressed -s \
+    -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -H 'Origin: http://www.flyland.ch'  \
+    -H 'Referer: http://www.flyland.ch/disclamer.php?case=1001' \
+    --data 'cbAkzeptieren=on' \ > raw.kml
+
+xmlstarlet sel -N "ns=http://www.opengis.net/kml/2.2" -t -v "/ns:kml/ns:Folder/ns:Folder/ns:name/." raw.kml | sort > generated/_layers-available.log; cat generated/_layers-available.log
 xmlstarlet ed -N "ns=http://www.opengis.net/kml/2.2" -d "/ns:kml/ns:Folder/ns:Folder/ns:name[not(contains(text(),'Sonderregelung'))]/.." raw.kml > generated/sonderregelung.kml
 xmlstarlet ed -N "ns=http://www.opengis.net/kml/2.2" -d "/ns:kml/ns:Folder/ns:Folder/ns:name[not(contains(text(),'Hindernisse'))]/.." raw.kml > generated/hindernisse.kml
 xmlstarlet ed -N "ns=http://www.opengis.net/kml/2.2" -d "/ns:kml/ns:Folder/ns:Folder/ns:name[not(contains(text(),'CTR'))]/.." raw.kml > generated/ctr.kml
